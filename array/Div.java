75fa3d0e84dbd53de422743a84c999a3ce64b707
PoojaRangrez <89906169+PoojaRangrez@users.noreply.github.com>
2021-09-21 09:05:18 +0530

    Add files via upload

---
+++ array/Div.java
@@ -0,0 +1,15 @@
+package com.xworkz.array;
+
+public class Div {
+
+	public static void main(String[] args) {
+		int test[]= {1,2,3,4,5,6,7,8,9,10};
+		for(int i=0;i<test.length;i++) {
+			if(i%3==0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
